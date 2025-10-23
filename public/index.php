c288c36a6610a9bac67cd2ea2a78e2813c3c90b7
AngelB25 <a.boissie@lyon.ort.asso.fr>
2025-10-23 09:54:40 +0200

    page accueil

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <title>Accueil — Équipe dév</title>
+</head>
+<body>
+  <div class="container">
+    <header>
+      <h1>Page accueil — Équipe développement</h1>
+      <p>Choisir une page ci-dessous :</p>
+    </header>
+    
+    <div class="php-info">
+        <?php
+            
+        ?>
+    </div>
+
+    <div class="links">
+      <!-- Lien vers une page développeurs -->
+       <a href="../pages/angel.php">Angel</a>
+    </div>
+  </div>
+</body>
+</html>
